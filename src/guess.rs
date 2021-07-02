824468ac3996aa07e03f3a0a20eadf72ca95ccfe
Jonathan Hyland <85582235+jonathan-hyland@users.noreply.github.com>
2021-07-02 15:19:54 -0400

    Implemented repeat games. Fixed #6

--- src/guess.rs
+++ src/guess.rs
@@ -13,10 +13,13 @@ pub mod guess {
 
     impl Guess {
 
-        pub fn new_guess() {
+        pub fn new_guess() -> bool {
+            let mut finished = false;
             let mut user_guess: Guess;  
             let mut guessing = true;
             let random_number = guess_functions::guess_functions::gen_rand_num();
+
+            println!("{}", random_number);
 
             while guessing == true {
                 let user_input = guess_functions::guess_functions::ask_guess();
@@ -30,6 +33,7 @@ pub mod guess {
                         if user_guess.number.trim().parse::<i32>() == Ok(user_guess.rand_num) {
                             println!("Your guess was correct!");
                             guessing = false;
+                            finished = true;
                         } else {
                             println!("Wrong guess. Guess again!");
                             guessing = true;
@@ -38,6 +42,7 @@ pub mod guess {
                     guessing = true;
                 };
             };
+        finished
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
 mod guess;
+use std::io;
 
 fn main() {
-    println!("Welcome to the Guesser!");
-    println!("Type in your guess, which should be a number from 1 to 100.");
-    guess::guess::Guess::new_guess();
+    let mut the_game = true;
+
+    while the_game == true {
+        println!("Welcome to the Guesser!");
+        println!("Type in your guess, which should be a number from 1 to 100.");
+        
+        let guessing_game = guess::guess::Guess::new_guess();
+
+        if guessing_game == true {
+            let mut new_game = String::new();
+            let mut initialize_game = false;
+
+            while initialize_game == false {
+                println!("The game is over! Would you like to play again? y/n");
+                io::stdin()
+                    .read_line(&mut new_game)
+                    .expect("Input could not be read.");
+                    if new_game.trim() == "y" {
+                        initialize_game = true;
+                        the_game = true;
+                    } else if new_game.trim() == "n" {
+                        initialize_game = true;
+                        the_game = false;
+                        println!("Thanks for playing!");
+                    } else {
+                        new_game.clear();
+                        initialize_game = false;
+                        println!("Please enter y or n.");
+                    }
+            }
+        } else {
+            the_game = false;
+            println!("Thanks for playing!");
+        }
+    }
 }
